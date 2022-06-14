69409a9cccb523468beb17e10ac7e39851708a64
“Sylphrena0” <kirkkleinsasser@gmail.com>
2022-06-14 13:06:41 -0400

    removed useless comments

--- run-compute-farm.sh
+++ run-compute-farm.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-#This is a template script for use in the batch queueing system. Please only edit the user sections.
+#this is a template script for use in the batch queueing system. Please only edit the user sections.
+#send files to compute farm: https://wiki.classe.cornell.edu/Computing/GridEngine
 LOCAL_DIR=${TMPDIR}
 ################USER SETTINGS###########################
-#You can edit the variables here, but something valid must be present.
-# SOURCE_DIR defaults to a files subdirectory of the directory you submitted the job from
-#Comment out to not import any files for the job. Edit for your specific needs.
+#you can edit the variables here, but something valid must be present.
+#SOURCE_DIR defaults to a files subdirectory of the directory you submitted the job from
+#comment out to not import any files for the job. Edit for your specific needs.
 SOURCE_DIR=${SGE_O_WORKDIR}   
 DESTINATION_DIR=/cdat/tem/sge/${USER}/${JOB_ID}
 ########################################################
@@ -12,11 +13,12 @@ if [ ! -z "${SOURCE_DIR}" ]; then
    rsync -avz ${SOURCE_DIR}/ ${LOCAL_DIR}
 fi
 
-#Put your code in the user section below. You can delete the entire
-#section between USER SECTION and END USER SECTION - it is a very simple
-#example script that does a loop and echos some job data for testing.
-#################USER SECTION###########################
-python3
+#put your code in the user section below. 
+#################USER SECTION########################### 
+
+echo "Starting Script!"
+python3 ./get_featurizers.py
+
 #################END USER SECTION#######################
 mkdir -p ${DESTINATION_DIR}
 ls ${DESTINATION_DIR}
