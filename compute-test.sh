30bdf49b33f5c36a51132cc28defedb5d6fad069
“Sylphrena0” <kirkkleinsasser@gmail.com>
2022-07-29 19:12:48 -0400

    condensed testing scripts

--- compute-test.sh
+++ compute-test.sh
@@ -10,40 +10,40 @@
 
 #read the options
 TEMP=`getopt -o pf:c::m::n::e:: --long pythonfile:,cores::,memory::,notify::,email:: -- "$@" command 2> /dev/null` #command 2> /dev/null suppresses unknown argument errors here
-echo "start loop"
+eval set -- "$TEMP"
+
 #extract options and their arguments into variables.
 while true ; do
     case "$1" in
         -pf|--pythonfile)
-            file=$2 ; echo $1; shift 2 ;;
+            file=$2 ; shift 2 ;;
         -c|--cores)
             case "$2" in
-                "") cores='32';  echo $1; shift 2 ;;
-                 *) cores=$2; echo $1; shift 2 ;;
+                "") cores='32' ; shift 2 ;;
+                 *) cores=$2 ; shift 2 ;;
             esac ;;
         -m|--memory)
             case "$2" in
-                "") memory='32G' ; echo $1; shift 2 ;;
-                 *) memory=$2 ; echo $1; shift 2 ;;
+                "") memory='32G' ; shift 2 ;;
+                 *) memory=$2 ; shift 2 ;;
             esac ;;
         -n|--notify)
             case "$2" in
-                "") notify='e' ; echo $1; shift 2 ;;
-                 *) notify=$2 ; echo $1; shift 2 ;;
+                "") notify='e' ; shift 2 ;;
+                 *) notify=$2 ; shift 2 ;;
             esac ;;
         -e|--email)
             case "$2" in
-                "") email='kvk23@cornell.edu' ; echo $1; shift 2 ;;
-                 *) email=$2 ; echo $1; shift 2 ;;
+                "") email='kvk23@cornell.edu' ; shift 2 ;;
+                 *) email=$2 ; shift 2 ;;
             esac ;;
-        --) shift ; echo "break '--'"; break ;; #breaks while loop when we run out of arguments
-        "") shift ; echo "break ''"; break ;; #breaks while loop when we run out of arguments
-        *) echo "last defined argument"; break ;;
+        --) shift ; break ;; #breaks while loop when we run out of arguments
+        *) echo "Internal error!" ; exit 1 ;;
     esac
 done
-echo "loop done"
+
 #passes variable to set the run time name of the job
-#$ -N compute.results
+#$ -N train_single.results
 #
 #set the queue
 #$ -q all.q
@@ -54,17 +54,27 @@ echo "loop done"
 #to make sure that the error.e and output.o files arrive in the current working directory
 #$ -cwd
 #
+#send an email when the job ends (b[egin], e[nd], a[bort])
+#$ -m $notify -M $email
+#
+#set thread requirements
+#$ -pe sge_pe $cores
+#
+#set memory requirements
+#$ -l mem_free=$memory
+#
 #combine error and output files into single file
 #$ -j y
 #
+
 #print some environment information - for diagnostics
 echo "Job ${JOBNAME} Starting at: "`date`
 echo "Running with command \"compute run_single_training.sh "$@"\" on host: "`hostname`
 #
 #run the python script
 source /opt/rh/rh-python38/enable
-export PYTHONPATH=/nfs/opt/python3.8/packages.sl7/lib/python3.8/site-packages:/nfs/opt/python3.8/packages.sl7:/home/kvk23/.local/lib/python3.8/site-packages #use local python packages, installed with pip3 install --user <package>
-#python3 ./code/$file $@ # pass inputs to python (limits and enabled types)
+# export PYTHONPATH=/nfs/opt/python3.8/packages.sl7/lib/python3.8/site-packages:/nfs/opt/python3.8/packages.sl7:/home/kvk23/.local/lib/python3.8/site-packages #use local python packages, installed with pip3 install --user <package>
+python3 ./code/$file $@ # pass inputs to python (limits and enabled types)
 
 # Documentation:
 # https://wiki.classe.cornell.edu/Computing/GridEngine - CLASSE wiki
--- compute.sh
+++ compute.sh
@@ -44,6 +44,18 @@ done
 
 exec "qsub" -N $file -q all.q -S /bin/bash -cwd -m $notify -M $email -pe sge_pe $cores -l mem_free=$memory -j y compute_run.sh $@
 
+==> compute_run.sh <== #would be in a seperate file, but this doesn't work (I don't know why) so I don't want the clutter!
+#print some environment information for diagnostics:
+echo "Job ${JOBNAME} Starting at: "`date`
+echo "Running with command \"compute run_single_training.sh "$@"\" on host: "`hostname`
+#use specific version of python
+source /opt/rh/rh-python38/enable
+#use local python packages, installed with pip3 install --user <package>:
+export PYTHONPATH=/nfs/opt/python3.8/packages.sl7/lib/python3.8/site-packages:/nfs/opt/python3.8/packages.sl7:/home/kvk23/.local/lib/python3.8/site-packages 
+#run the python script:
+python3 ./code/$1.py $@ #the $@ passes remaining arguments to python
+==>               <== 
+
 # Documentation:
 # https://wiki.classe.cornell.edu/Computing/GridEngine - CLASSE wiki
 # https://mail.google.com/mail/u/0/#inbox/FMfcgzGpGTHKnnfqXlTLRRSKVzDfDqrP - Cornell IT explaination of submiting items to queue while using python libraries
